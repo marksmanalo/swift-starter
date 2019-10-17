42422216ab21722ffe1441ed8420a660e48e61ac
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2019-10-16 23:16:15 -0700

    Initial checkin

---
+++ swift-starter/swift-starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-starter
+//
+//  Created by Mark S. Manalo on 10/16/19.
+//  Copyright © 2019 Mark S. Manalo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
