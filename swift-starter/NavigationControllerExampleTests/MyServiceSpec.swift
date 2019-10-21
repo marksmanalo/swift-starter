5923ca4a9fff7d99c2bcaa98226e1b506e9bd9d3
mmanalo <marksmanalo@marks-mbp-2.attlocal.net>
2019-10-20 18:36:57 -0700

    finished adding the testing framework dependencies

---
+++ swift-starter/NavigationControllerExampleTests/MyServiceSpec.swift
@@ -0,0 +1,33 @@
+@testable import NavigationControllerExample
+import Foundation
+import Cuckoo
+import Nimble
+import Quick
+import RxSwift
+import RxTest
+
+class NotificationCenterAPISpec: QuickSpec {
+
+  override func spec() {
+
+    describe("A service") {
+
+      //var myService: MyService!
+
+      context("when doing stuff") {
+        
+        beforeEach {
+          
+        }
+        
+        it("should do the stuff") {
+          
+        }
+
+      }
+
+    }
+
+  }
+
+}
---
+++ swift-starter/NavigationControllerExampleTests/swift_starterTests.swift
@@ -0,0 +1,39 @@
+//
+//  swift_starterTests.swift
+//  swift-starterTests
+//
+//  Created by Mark S. Manalo on 10/19/19.
+//  Copyright © 2019 Mark Manalo. All rights reserved.
+//
+
+@testable import NavigationControllerExample
+import Foundation
+import Cuckoo
+import Nimble
+import Quick
+
+class MainSpec: QuickSpec {
+
+  override func spec() {
+    
+    describe("A service") {
+      
+      // var myservice = myservice()
+      
+      context("when loading up") {
+        
+        beforeEach {
+          
+        }
+        
+        it("should do some stuff") {
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+
+}
